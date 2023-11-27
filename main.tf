8ddab3169b739a8e26f6d5150e68781e4ad7cafc
Taha <yhasinmuhammad123@gmail.com>
2023-11-27 13:49:53 -0700

    added an instance

---
+++ main.tf
@@ -0,0 +1,30 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+variable "instance_type" {
+    description = "determines instance type"
+    type = string
+    default = "t2.micro"
+}
